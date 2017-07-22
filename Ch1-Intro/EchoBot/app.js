28607405a5171619fa0004e8949dd03b87e0f7e7
Eric ShangKuan <ericsk@gmail.com>
2017-07-22 13:00:04 +0800

    Echo 機器人的範例

---
+++ Ch1-Intro/EchoBot/app.js
@@ -0,0 +1,20 @@
+const restify = require('restify');
+const builder = require('botbuilder');
+
+// 建立 restify server 聽在 3978 port (或是指定的 port)
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// 建立 ChatConnector 接收由 Bot Connector 來的資訊
+let connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+server.post('/api/messages', connector.listen());
+
+// 建立 bot 實體來處理 chat connector 接收到的內容
+let bot = new builder.UniversalBot(connector, (session) => {
+  session.send(`您說「${session.message.text}」`);
+});
